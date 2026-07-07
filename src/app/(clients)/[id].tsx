e71d4de66bc4b7e6c59099509b7b67d21cf3a2c0
veraxqy <elderbruno2014@gmail.com>
2026-07-07 00:11:03 -0300

    new: Integração do Back com o Front - Cliente Detalhado

--- src/app/(clients)/[id].tsx
+++ src/app/(clients)/[id].tsx
@@ -1,25 +1,63 @@
 import { Button } from "@/components/Button"
+import { useLocalSearchParams } from "expo-router"
 import { useRouter } from "expo-router"
-import { View, Text, ScrollView } from "react-native"
+import { View, Text, ScrollView, Modal } from "react-native"
+import { useEffect, useState } from "react"
+import { getClients, Client } from "@/services/api"
 
-export default function Client(){
+export default function ClientDetails(){
+
+    const [showDeleteModal, setShowDeleteModal] = useState(false)
+    const { id } = useLocalSearchParams()
+    const [client, setClient] = useState<Client | null>(null)
+
     const router = useRouter()
+
+    useEffect(() => {
+
+        async function loadClient() {
+            try {
+                const data = await getClients()
+                const clientFound = data.find((item) => String(item.id) === String(id))
+                setClient(clientFound ?? null)
+            } catch (error) {
+                console.error("Erro ao buscar cliente:", error)
+            }
+        }
+
+        if(id){
+            loadClient()
+        }
+
+    }, [id])
+
+    if(!client){
+        return(
+            <View className="w-full h-full items-center justify-center">
+                <Text className="text-preto text-2xl font-bold">Carregando cliente...</Text>
+            </View>
+        )
+    }
+       
     return (
         <View className="w-full h-full">
             <ScrollView>
                 <View className="w-full px-10 h-full items-center self-center gap-5 py-10">
-                    <View className="bg-branco-100 w-full rounded-lg p-5 gap-4 border border-cinza-200">
-                        <View className="flex-row justify-between">
-                            <Text className="self-start text-preto font-bold text-2xl">Cliente #ID</Text>
-                            <Text className="self-start text-cinza-200 font-bold text-2xl">00/00/0000</Text>
+                    <View className="bg-branco-100 w-full rounded-xl pt-8 p-5 gap-4 border border-cinza-200">
+                        <View className="flex-row gap-2 justify-between items-center">
+                            <Text className="self-start text-preto font-bold text-2xl">Nome: {client.nome}</Text>
+                            <Text className="self-start text-preto font-bold text-2xl">#{client.id}</Text>
                         </View>
-                        <Text className="self-start text-preto font-normal text-2xl">Nome:</Text>
                         <Text className="self-start text-preto font-normal text-2xl">Telefone:</Text>
-                        <Text className="self-start text-preto font-normal text-2xl">Endereço:</Text>
-                        <View className="w-full gap-4">
-                            <Button label="Editar" color="text-branco-100" background="bg-vermelho-200"/>
-                            <Button className="border-2 border-vermelho-100 rounded-lg" label="Excluir" color="text-vermelho-100" background="bg-transparent"/>
-                        </View>
+                        <Text className="self-start text-preto font-bold text-2xl">{client.telefone}</Text>
+                        <Text className="self-start text-preto font-normal text-2xl">Email:</Text>
+                        <Text className="self-start text-preto font-bold text-2xl">{client.email}</Text>
+                        <Text className="self-start text-preto font-normal text-2xl">Endereço: </Text>
+                        <Text className="self-start text-preto font-bold text-2xl">{client.logradouro}, {client.numero}, {client.bairro}</Text>
+                    </View>
+                    <View className="w-full gap-4">
+                        <Button label="Editar" color="text-branco-100" background="bg-vermelho-200"/>
+                        <Button className="border-2 border-vermelho-100 rounded-lg" label="Excluir" color="text-vermelho-100" background="bg-transparent"/>
                     </View>
                 </View>
             </ScrollView>
